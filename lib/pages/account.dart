23756b646da4242c3d690f683f801ee7e608ff95
Jack <a0930522803@gmail.com>
2024-05-09 13:38:59 +0800

    Fix bugs of account and remove legacy page

--- lib/pages/account.dart
+++ lib/pages/account.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:diet_tracker/widgets/app_bar.dart';
 import 'package:diet_tracker/utils/style.dart';
-import 'package:diet_tracker/widgets/people_card.dart';
 import 'package:diet_tracker/widgets/post_card.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:diet_tracker/widgets/people_card.dart';
+// import 'package:flutter/widgets.dart';
 
 class AccountPage extends StatefulWidget {
   const AccountPage({super.key});
@@ -36,35 +38,163 @@ class _AccountPageState extends State<AccountPage> {
     return Scaffold(
       appBar: const MyAppBar(title: 'Account'),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            const PeopleBlock(photo: 'assets/headshot.png', name: "Jack", gender: "Male"),
-            SizedBox(height: size.height * 0.02),
-            SizedBox(height: size.height * 0.06, child: 
-              const CustomText(label: "Posts", type: 'titleLarge')
-            ),
-            SizedBox(height: size.height * 0.02),
-            SizedBox(height: size.height * 0.5, child: 
-              CustomScrollView(
-                slivers: <Widget>[
-                  SliverList(
-                    delegate: SliverChildListDelegate(
-                      <Widget>[
-                        ..._postList.isEmpty 
-                          ? [Column(children:[
-                                SizedBox(height: size.height * 0.3), 
-                                const CustomText(label: "No entries yet.", 
-                                            type: 'displaySmall',
-                                          )]
-                            )]
-                          : _postList,
-                      ],
+        child: Flex(
+          direction: Axis.horizontal,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Stack(
+                children: [
+                  Container(
+                    alignment: Alignment.topCenter,
+                    decoration: const BoxDecoration(
+                      color: Colors.white12,
+                      image: DecorationImage(
+                        image: AssetImage('assets/banner.jpg'),
+                        fit: BoxFit.cover,
+                        alignment: Alignment.topCenter
+                      ),
                     ),
+                    height: size.height * 0.4,
                   ),
-                ],
+                  Positioned(
+                    left: size.width * 0.05,
+                    top: size.height * 0.3,
+                    child: Column(
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            SizedBox(width: size.width*0.02),
+                            CircleAvatar(
+                              backgroundColor: Colors.black12,
+                              radius: size.height * 0.12,
+                              child: CircleAvatar(
+                                backgroundImage:const AssetImage('assets/headshot.png'),
+                                radius: size.height * 0.1,
+                              ),
+                            ),
+                            SizedBox(width: size.width*0.005),
+                            const Text("Jack",
+                              style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 2.0,
+                              ), 
+                            ),
+                            SizedBox(width: size.width*0.01),
+                            
+                            // const Padding(
+                            //   padding: EdgeInsets.only(bottom: 8),
+                            //   child: Icon(Icons.male, color: Colors.black38,),
+                            // ),
+                          ],
+                        ),
+                        SizedBox(height: size.height*0.06),
+                        Row(
+                          children: [
+                            SizedBox(width: size.width*0.04),
+                            const Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "2",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight:FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                SizedBox(height: 5),
+                                Text(
+                                  "Posts",
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight:FontWeight.normal,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(width: size.width*0.025),
+                            const Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "10",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight:FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                SizedBox(height: 5),
+                                Text(
+                                  "Likes",
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight:FontWeight.normal,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(width: size.width*0.025),
+                            const Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "1200",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight:FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                SizedBox(height: 5),
+                                Text(
+                                  "Calories",
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight:FontWeight.normal,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          
+                          ],
+                        )
+                        
+                      ]
+                    )
+                  ),
+                  Container(
+                    height: size.height*0.8,
+                  )
+                ]
               ),
             ),
+            Expanded(
+              flex: 2,
+              child:_postList.isEmpty ? 
+                const Center(
+                  child: CustomText(label: "No entries yet.", 
+                                type: 'displaySmall',)
+                  ):
+                SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: SizedBox(
+                    height: size.height * 0.9, 
+                    child: ListView.builder(
+                      itemCount: _postList.length,
+                      itemBuilder: (context, index){
+                        return _postList[index];
+                      }
+                    ),
+                  )
+                )
+            )
           ],
         ),
       ),
